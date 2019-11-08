56376f7802583ccfcc56ce186336e48e2d3080d8
sneha.raza <sneha.raza@ucdenver.edu>
2019-11-08 10:04:54 -0700

    11\18\2019 work

--- sample code.py
+++ sample code.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 from tslearn.clustering import TimeSeriesKMeans
 from tslearn.datasets import CachedDatasets
-from tslearn.preprocessing import TimeSeriesScalerMeanVariance, \
+from tslearn.preprocessing import TimeSeriesScalerMeanVariance, 
     TimeSeriesResampler
 
 with open('data.json') as json_file:  
@@ -20,33 +20,45 @@ with open('data.json') as json_file:
 
 data = pd.DataFrame(json_data['data'])
 #data.loc[:, ['PAS1']]
-np.shape(data.loc[:, ['PAS1']])
+np.shape(['PAS1'])
 plt.plot(data['PAS1'][1])
 plt.show()
 
 
+dat = data.loc[:, ['PAS1']].to_numpy()
 
+ts_dat = np.zeros((18, 4096))
+for idx, trace in enumerate(dat):
+    print (trace[0])
+    ts_dat[idx] = trace[0]
+
+np.shape(ts_dat)
+
+X_train= ts_dat
 
 
 seed = 0
 np.random.seed(seed)
-X_train, y_train, X_test, y_test = CachedDatasets().load_dataset("Trace")
-X_train = X_train[y_train < 4]  # Keep first 3 classes
+#X_train, y_train, X_test, y_test = CachedDatasets().load_dataset("Trace")
+#X_train = X_train[y_train < 4]  # Keep first 3 classes
 np.random.shuffle(X_train)
+
 # Keep only 50 time series
-X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train[:50])
+X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train)
+
 # Make time series shorter
-X_train = TimeSeriesResampler(sz=40).fit_transform(X_train)
+X_train = TimeSeriesResampler(sz=100).fit_transform(X_train)
 sz = X_train.shape[1]
 
 # Euclidean k-means
 print("Euclidean k-means")
-km = TimeSeriesKMeans(n_clusters=3, verbose=True, random_state=seed)
+n_clusters=3
+km = TimeSeriesKMeans(n_clusters=n_clusters, verbose=True, random_state=seed)
 y_pred = km.fit_predict(X_train)
 
 plt.figure()
-for yi in range(3):
-    plt.subplot(3, 3, yi + 1)
+for yi in range(n_clusters):
+    plt.subplot(n_clusters, n_clusters, yi + 1)
     for xx in X_train[y_pred == yi]:
         plt.plot(xx.ravel(), "k-", alpha=.2)
     plt.plot(km.cluster_centers_[yi].ravel(), "r-")
@@ -57,7 +69,7 @@ for yi in range(3):
 
 # DBA-k-means
 print("DBA k-means")
-dba_km = TimeSeriesKMeans(n_clusters=3,
+dba_km = TimeSeriesKMeans(n_clusters=n_clusters,
                           n_init=2,
                           metric="dtw",
                           verbose=True,
